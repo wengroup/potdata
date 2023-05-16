958d8eb0f9e7c1c3674cc1d8969cc51352933587
mjwen <wenxx151@gmail.com>
2023-05-15 23:00:22 -0500

    Add tests for strain and perturbation transformation

---
+++ tests/structure/test_transformations.py
@@ -0,0 +1,39 @@
+import numpy as np
+from potdata.transformations import PertubationTransformation, StrainTransformation
+
+
+def test_strain_transformation(Si_structure):
+    st = StrainTransformation(conventional=False)
+    transformed_structures = st.apply_transformation(Si_structure)
+    assert len(transformed_structures) == 7 * 4
+
+    st = StrainTransformation(conventional=True)
+    transformed_structures = st.apply_transformation(Si_structure)
+    # only 2 for shear, because, for example, -0.1 and 0.1 are the same for shear
+    assert len(transformed_structures) == 10
+
+    for d in transformed_structures:
+        assert "structure" in d
+        assert "deformation" in d
+
+
+def test_pertubation_transformation(Si_structure):
+    num_struct = 10
+    low = 0.1
+    high = 0.3
+    pt = PertubationTransformation(num_structures=num_struct, low=low, high=high)
+    transformed_structures = pt.apply_transformation(Si_structure)
+
+    # only select the atom whose initial position is not (0, 0, 0).
+    # the (0, 0, 0) atom can be perturbed to the other side of the cell due to PBC.
+    orig_coords = Si_structure.cart_coords[[0]]
+
+    assert len(transformed_structures) == num_struct
+    for d in transformed_structures:
+        s = d["structure"]
+        new_coords = s.cart_coords[[0]]
+
+        distances = np.linalg.norm(new_coords - orig_coords, axis=1)
+
+        assert np.max(distances) <= high
+        assert np.min(distances) >= low
